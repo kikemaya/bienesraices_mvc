778386e43df5d6ac138b9dee4606c74a94708194
kikemaya <enrique.mg.cd@gmail.com>
2024-05-20 14:58:20 -0600

    first user validations added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,7 +20,29 @@ const registerUser = async (req, res) => {
     .withMessage("Name value cannot be empty")
     .run(req);
 
+  await check("email")
+    .isEmail()
+    .withMessage("The email format is not valid")
+    .run(req);
+
+  await check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password should contain at least 6 characters")
+    .run(req);
+
+  await check("repeat_password")
+    .equals("password")
+    .withMessage("Passwords are not identical")
+    .run(req);
+
   let result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    return res.render("auth/register", {
+      page: "Create account",
+      errores: result.array(),
+    });
+  }
 
   res.json(result.array());
 

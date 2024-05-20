95f2efdb76ac6300fae61f6c4e7d378042ccfa88
kikemaya <enrique.mg.cd@gmail.com>
2024-05-19 21:13:44 -0600

    'User' model, created. Not tested yet

---
+++ models/User.js
@@ -0,0 +1,22 @@
+import { DataTypes } from "sequelize";
+
+import db from "./../config/db.js";
+
+const User = db.define("users", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  token: DataTypes.STRING,
+  confirmated: DataTypes.BOOLEAN,
+});
+
+export default User;
